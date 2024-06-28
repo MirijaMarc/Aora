9b2b3bed84d84ca42eeb7c671a2a6f451dcc9a70
MirijaMarc <mirijarazafimbelo30@gmail.com>
2024-06-28 13:18:12 +0300

    [init]

---
+++ app/(auth)/_layout.jsx
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Stack } from 'expo-router'
+
+export default function AuthLayout() {
+  return (
+    <>
+      <Stack>
+          <Stack.Screen 
+            name='sign-in'
+            options={{
+              headerShown: false
+            }}
+          />
+          <Stack.Screen 
+            name='sign-up'
+            options={{
+              headerShown: false
+            }}
+          />
+      </Stack>
+    </>
+  )
+}
